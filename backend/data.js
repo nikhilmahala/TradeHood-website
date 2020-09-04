cff8182a247348f872b9e5074c390d218a03393d
nikhil singh mahala <mynamenikhil44@gmail.com>
2020-09-05 00:50:56 +0530

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 600,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'Regular fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 500,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Skinny fit Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 700,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      name: 'Slim fit Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 700,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    },
+  ]
+}
